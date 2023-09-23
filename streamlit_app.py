1482c53145073bf2dfa0f05d4a5de81612ec6164
morispolanco <morispolanco@gmail.com>
2023-09-23 11:49:18 -0600

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,47 @@
+import openai
+import streamlit as st
+
+def assistant():
+    with st.sidebar:
+        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+        "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+        
+    st.title("Análisis de competencia lingüística")
+
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
+
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+
+    prompt = "Realiza un diagnóstico de la competencia lingüística del autor del texto evaluando gramática, ortografía, puntuación, vocabulario y estructura de oraciones. Examina su capacidad para comunicar ideas y uso de estilos y tonos según el contexto. Usa ejemplos del texto para respaldar tus hallazgos y, si encuentras áreas mejorable, sugiere recomendaciones apropiadas para mejorar su competencia."
+    
+    if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    user_input = st.text_area("Escriba tres párrafos sobre cualquier tema", height=300)
+
+    if user_input:
+        st.session_state.messages.append({"role": "user", "content": user_input})
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+        msg = response.choices[0].message
+        st.session_state.messages.append(msg)
+        st.chat_message("assistant").write(msg.content)
+
+    if st.button("Analizar"):
+        optimized_prompt = generate_optimized_prompt(prompt)
+        st.session_state.messages.append({"role": "user", "content": optimized_prompt})
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+        msg = response.choices[0].message
+        st.session_state.messages.append(msg)
+        st.chat_message("assistant").write(msg.content)
+
+def generate_optimized_prompt(prompt):
+    # Aquí puedes agregar tu lógica para generar el prompt optimizado
+    optimized_prompt = prompt + " [Optimized]"
+    return optimized_prompt
+
+assistant()
