e1483791387c0e6f6f8ddfc01f931a29de8ed8b1
morispolanco <morispolanco@gmail.com>
2023-09-23 12:07:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 import openai
 import streamlit as st
-import pyttsx3
 
 def assistant():
     with st.sidebar:
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
         
-    st.title("Análisis de competencia lingüística")
+    st.title("Consultas lingüísticas")
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = []
@@ -18,13 +17,11 @@ def assistant():
 
     openai.api_key = openai_api_key
 
-    prompt = "Realiza un diagnóstico de la competencia lingüística del autor del texto evaluando gramática, ortografía, puntuación, vocabulario y estructura de oraciones. Examina su capacidad para comunicar ideas y uso de estilos y tonos según el contexto. Usa ejemplos del texto para respaldar tus hallazgos y, si encuentras áreas mejorable, sugiere recomendaciones apropiadas para mejorar su competencia."
-    
+    prompt = "Como consultor experto en español, tu objetivo es proporcionar asesoramiento y orientación especializada en diversos temas relacionados con el idioma español. Tu tarea consiste en ofrecer información precisa, útil y de calidad a aquellos que necesiten asistencia en el ámbito del español."
     if prompt:
         st.session_state.messages.append({"role": "user", "content": prompt})
-        text_to_speech(prompt)
 
-    user_input = st.text_area("Escriba tres párrafos sobre cualquier tema", height=300)
+    user_input = st.text_area("Escriba su pregunta sobre el idioma español", height=150)
 
     if user_input:
         st.session_state.messages.append({"role": "user", "content": user_input})
@@ -32,27 +29,18 @@ def assistant():
         msg = response.choices[0].message
         st.session_state.messages.append(msg)
         st.chat_message("assistant").write(msg.content)
-        text_to_speech(msg.content)
 
-    if st.button("Analizar"):
+    if st.button("Consultar"):
         optimized_prompt = generate_optimized_prompt(prompt)
         st.session_state.messages.append({"role": "user", "content": optimized_prompt})
         response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
         msg = response.choices[0].message
         st.session_state.messages.append(msg)
         st.chat_message("assistant").write(msg.content)
-        text_to_speech(msg.content)
 
 def generate_optimized_prompt(prompt):
     # Aquí puedes agregar tu lógica para generar el prompt optimizado
     optimized_prompt = prompt + " [Optimized]"
     return optimized_prompt
-
-def text_to_speech(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 150)  # Velocidad de reproducción del habla
-    engine.setProperty('volume', 0.8)  # Volumen del habla
-    engine.say(text)
-    engine.runAndWait()
 
 assistant()
